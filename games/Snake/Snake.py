4a492fc300a67a5cb37642962e7d4e94f6700074
Ackring <hugo.ackring@gmail.com>
2021-01-14 14:20:29 +0100

    Snake game

---
+++ games/Snake/Snake.py
@@ -0,0 +1,13 @@
+from libs.SimpleArcade import Game
+
+class Snake(Game.Game):
+
+    def __init__(self):
+        super().__init__("Snake", (None))
+
+    def onPlay(self):
+        pass
+
+    def update(self, screen):
+        pass
+    
