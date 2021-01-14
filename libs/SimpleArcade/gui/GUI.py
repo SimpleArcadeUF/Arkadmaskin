6caa7f4d0f18eed44e3f724a4a6533baa465c1ae
Edvin Andersson <simplearcadeuf@gmail.com>
2021-01-14 14:04:10 +0100

    .

--- libs/SimpleArcade/gui/GUI.py
+++ libs/SimpleArcade/gui/GUI.py
@@ -1,6 +1,8 @@
 import pygame
 from libs.SimpleArcade import Arcade
 
+GUI_IS_CLICKED = False
+
 class GUI:
 
     def __init__(self, x=0, y=0, width=100, height=100, bgColor=None):
@@ -185,7 +187,7 @@ class GUI:
     def _setIsClicked(self):
         self._clicked = False
         if(Arcade.PLATFORM == Arcade.PLATFORM_DESKTOP):
-            if(self._hovered and pygame.mouse.get_pressed()[0]):
+            if(self._hovered and pygame.mouse.get_pressed()[0] and GUI_IS_CLICKED == False):
                 self._clicked = True
         elif(Arcade.PLATFORM == Arcade.PLATFORM_ARCADE):
             if(self._hovered and Arcade.BUTTON_PRESSED_1):
