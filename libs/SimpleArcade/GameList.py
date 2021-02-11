2d8a9769c7c75d5e7d7b9fe56077a2298057ae33
Huto <thedampa222@gmail.com>
2021-02-11 13:32:34 +0100

    Lade till Faestone till gameslist

--- libs/SimpleArcade/GameList.py
+++ libs/SimpleArcade/GameList.py
@@ -1,12 +1,14 @@
 from games.Pong import Pong
 from games.Pacman import Pacman
 from games.Escape import Escape
+from games.Faestone import Faestone
 
 GAMES = []
 
 def initGames():
     global GAMES
 
+    GAMES.append( Faestone.Faestone() )
     GAMES.append( Pacman.Packman() )
     GAMES.append( Pong.Pong() )
     GAMES.append( Escape.Escape() )
