0acc33a4dfb7ab308fb23c51878f6e0ce3f8f9db
Edvin Andersson <simplearcadeuf@gmail.com>
2021-02-18 15:11:59 +0100

    fix

--- libs/SimpleArcade/GameList.py
+++ libs/SimpleArcade/GameList.py
@@ -1,11 +1,8 @@
 from games.Pong import Pong
 from games.Pacman import Pacman
-<<<<<<< HEAD
 from games.Snake import Snake
-=======
 from games.Escape import Escape
 from games.Faestone import Faestone
->>>>>>> 85405a328cdd8440020ce00552c4197c0b63fe92
 
 GAMES = []
 
@@ -14,9 +11,6 @@ def initGames():
 
     GAMES.append( Faestone.Faestone() )
     GAMES.append( Pacman.Packman() )
-<<<<<<< HEAD
     GAMES.append( Snake.Snake() )
-=======
     GAMES.append( Pong.Pong() )
     GAMES.append( Escape.Escape() )
->>>>>>> 85405a328cdd8440020ce00552c4197c0b63fe92
