7f9479dd54a6ee1873d1bb208703deb8a580b0a5
Edvin Andersson <simplearcadeuf@gmail.com>
2021-03-04 14:00:14 +0100

    Fixed

--- libs/SimpleArcade/GameList.py
+++ libs/SimpleArcade/GameList.py
@@ -3,12 +3,16 @@ from games.Pacman import Pacman
 from games.Snake import Snake
 from games.Escape import Escape
 from games.Faestone import Faestone
+from games.Bloons import Bloons
+from games.GeoDash import GeoDash
 
 GAMES = []
 
 def initGames():
     global GAMES
 
+    GAMES.append( GeoDash.GeoDash() )
+    GAMES.append( Bloons.Bloons() )
     GAMES.append( Faestone.Faestone() )
     GAMES.append( Pacman.Packman() )
     GAMES.append( Snake.Snake() )
