a55b484fd2270a873d2a16a47cb79dbc7d1ca38a
Lucas <lucas.persson.ohlin@hotmail.com>
2021-03-11 17:38:48 +0100

    ö

--- games/GeoDash/states/GameState.py
+++ games/GeoDash/states/GameState.py
@@ -1,6 +1,6 @@
 from games.GeoDash.states import State 
 from games.GeoDash.utils import Handler
-from games.GeoDash.tileSystem import TileType
+from games.GeoDash.tileSystem import TileType, GeoPlayer
 from libs.SimpleArcade.gui import Label, Button, Frame
 from libs.SimpleArcade import Game, Arcade
 
@@ -16,6 +16,11 @@ class GameState():
 
         self.tiles = [] 
         self.loadMap("games/GeoDash/res/maps/level1")
+
+        # player_image = pygame.image.load('games/GeoDash/res/images/player_sprite.png').convert()
+
+        self.player_sprite = GeoPlayer.Player(1)
+        self.player_group = pygame.sprite.Group(player_sprite)
 
     def update(self, screen):
         for tile in self.tiles:
@@ -44,5 +49,6 @@ class Tile:
 
     def update(self, screen):
         screen.blit(self.tileType.image, (self.x * self.size, self.y * self.size))
+        screen.blit(self.player_group)
 
 
