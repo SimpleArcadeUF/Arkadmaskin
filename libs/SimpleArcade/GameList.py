42723c6b7a226f7a80efdfd281cb368e23173d29
Theo Esberg <esbergtheo@gmail.com>
2021-04-21 13:22:42 +0200

    fixes

--- libs/SimpleArcade/GameList.py
+++ libs/SimpleArcade/GameList.py
@@ -6,24 +6,18 @@ from games.Faestone import Faestone
 from games.Bloons import Bloons
 from games.GeoDash import GeoDash
 from games.PolygonArena import PolygonArena
-<<<<<<< HEAD
 from games.CookieClicker import CookieClicker
 from games.SpaceInvaders import SpaceInvaders
-=======
 from games.Titanic import Titanic
->>>>>>> 5ec2748df2f5cd5f193e0db5cedebe519ccdcfcc
 
 GAMES = []
 
 def initGames():
     global GAMES
     
-<<<<<<< HEAD
     GAMES.append( SpaceInvaders.SpaceInvaders() )
     GAMES.append( CookieClicker.CookieClicker() )
-=======
     GAMES.append( Titanic.Titanic() )
->>>>>>> 5ec2748df2f5cd5f193e0db5cedebe519ccdcfcc
     GAMES.append( GeoDash.GeoDash() )
     GAMES.append( Bloons.Bloons() )
     GAMES.append( PolygonArena.PolygyonArena() )
